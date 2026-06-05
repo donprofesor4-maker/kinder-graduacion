ab4819d983d96adb0c02ee7c3cef8ec241af3d64
cactusmultimedia <gavota_enlatado.1u@icloud.com>
2026-06-04 18:37:56 -0600

    Scrollaroids - Galería de graduación kindergarten

---
+++ datos.js
@@ -0,0 +1,54 @@
+const BASE_FOTOS = "../Downloads/imagenes/graduacion/kinder individuales/";
+
+const ninos = [
+  {
+    id: 1,
+    codigo: "ck01",
+    fotos: [
+      "1/DSC_2077.JPG", "1/DSC_2078.JPG", "1/DSC_2081.JPG", "1/DSC_2083.JPG",
+      "1/DSC_2085.JPG", "1/DSC_2086.JPG", "1/DSC_2089.JPG", "1/DSC_2091.JPG",
+      "1/DSC_2093.JPG", "1/DSC_2095.JPG", "1/DSC_2097.JPG", "1/DSC_2100.JPG",
+      "1/DSC_2102.JPG", "1/DSC_2104.JPG"
+    ]
+  },
+  { id: 2, codigo: "ck02", fotos: [] },
+  { id: 3, codigo: "ck03", fotos: [] },
+  { id: 4, codigo: "ck04", fotos: [] },
+  { id: 5, codigo: "ck05", fotos: [] },
+  { id: 6, codigo: "ck06", fotos: [] },
+  { id: 7, codigo: "ck07", fotos: [] },
+  { id: 8, codigo: "ck08", fotos: [] },
+  { id: 9, codigo: "ck09", fotos: [] },
+  { id: 10, codigo: "ck10", fotos: [] },
+  { id: 11, codigo: "ck11", fotos: [] },
+  { id: 12, codigo: "ck12", fotos: [] },
+  { id: 13, codigo: "ck13", fotos: [] },
+  { id: 14, codigo: "ck14", fotos: [] },
+  { id: 15, codigo: "ck15", fotos: [] },
+  { id: 16, codigo: "ck16", fotos: [] },
+  { id: 17, codigo: "ck17", fotos: [] },
+  { id: 18, codigo: "ck18", fotos: [] },
+  { id: 19, codigo: "ck19", fotos: [] },
+  { id: 20, codigo: "ck20", fotos: [] },
+  { id: 21, codigo: "ck21", fotos: [] }
+];
+
+const portadas = [
+  "DSC_2106_copia.jpg", "DSC_2137_copia.jpg", "DSC_2208_copia.jpg",
+  "DSC_2223_copia.jpg", "DSC_2248_copia.jpg", "DSC_2271_copia.jpg",
+  "DSC_2305_copia.jpg", "DSC_2314_copia.jpg", "DSC_2347_copia.jpg",
+  "DSC_2371_copia.jpg", "DSC_2401_copia.jpg", "DSC_2410_copia.jpg",
+  "DSC_2436_copia.jpg", "DSC_2463_copia.jpg", "DSC_2495_copia.jpg",
+  "DSC_2525_copia.jpg", "DSC_2553_copia.jpg", "DSC_2564_copia.jpg",
+  "DSC_2578_copia.jpg", "DSC_2595_copia.jpg", "DSC_2752_copia.jpg"
+];
+
+const captions = [
+  "Primeros pasos", "Sonrisas de alegría", "Momentos especiales",
+  "Amistades forjadas", "Aprendizajes", "Juegos y risas",
+  "Celebraciones", "Logros alcanzados", "Recuerdos inolvidables",
+  "Crecimiento", "Sueños cumplieron", "Familia unida",
+  "Orgullo y alegría", "Nuevos comienzos", "El día más importante",
+  "Bachilleratos", "Felicidades", "Éxito total",
+  "Gracias por todo", "Hasta siempre", "El futuro nos espera"
+];
