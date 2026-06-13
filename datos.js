b59a8dc09f580e799b793ebbd3e3e24ec036ea74
cactusmultimedia <gavota_enlatado.1u@icloud.com>
2026-06-12 20:21:52 -0600

    Optimizar imágenes para web, Escape cierra todos los modals, caption dentro de cada nino

--- datos.js
+++ datos.js
@@ -1,34 +1,30 @@
 const ninos = [
-  {
-    id: 1,
-    codigo: "ck01",
-    fotos: [
-      "1/DSC_2077.JPG", "1/DSC_2078.JPG", "1/DSC_2081.JPG", "1/DSC_2083.JPG",
-      "1/DSC_2085.JPG", "1/DSC_2086.JPG", "1/DSC_2089.JPG", "1/DSC_2091.JPG",
-      "1/DSC_2093.JPG", "1/DSC_2095.JPG", "1/DSC_2097.JPG", "1/DSC_2100.JPG",
-      "1/DSC_2102.JPG", "1/DSC_2104.JPG"
-    ]
-  },
-  { id: 2, codigo: "ck02", fotos: [] },
-  { id: 3, codigo: "ck03", fotos: [] },
-  { id: 4, codigo: "ck04", fotos: [] },
-  { id: 5, codigo: "ck05", fotos: [] },
-  { id: 6, codigo: "ck06", fotos: [] },
-  { id: 7, codigo: "ck07", fotos: [] },
-  { id: 8, codigo: "ck08", fotos: [] },
-  { id: 9, codigo: "ck09", fotos: [] },
-  { id: 10, codigo: "ck10", fotos: [] },
-  { id: 11, codigo: "ck11", fotos: [] },
-  { id: 12, codigo: "ck12", fotos: [] },
-  { id: 13, codigo: "ck13", fotos: [] },
-  { id: 14, codigo: "ck14", fotos: [] },
-  { id: 15, codigo: "ck15", fotos: [] },
-  { id: 16, codigo: "ck16", fotos: [] },
-  { id: 17, codigo: "ck17", fotos: [] },
-  { id: 18, codigo: "ck18", fotos: [] },
-  { id: 19, codigo: "ck19", fotos: [] },
-  { id: 20, codigo: "ck20", fotos: [] },
-  { id: 21, codigo: "ck21", fotos: [] }
+  { id: 1, codigo: "ck01", caption: "Primeros pasos", fotos: [
+    "1/DSC_2077.JPG", "1/DSC_2078.JPG", "1/DSC_2081.JPG", "1/DSC_2083.JPG",
+    "1/DSC_2085.JPG", "1/DSC_2086.JPG", "1/DSC_2089.JPG", "1/DSC_2091.JPG",
+    "1/DSC_2093.JPG", "1/DSC_2095.JPG", "1/DSC_2097.JPG", "1/DSC_2100.JPG",
+    "1/DSC_2102.JPG", "1/DSC_2104.JPG"
+  ]},
+  { id: 2, codigo: "ck02", caption: "Sonrisas de alegría", fotos: [] },
+  { id: 3, codigo: "ck03", caption: "Momentos especiales", fotos: [] },
+  { id: 4, codigo: "ck04", caption: "Amistades forjadas", fotos: [] },
+  { id: 5, codigo: "ck05", caption: "Aprendizajes", fotos: [] },
+  { id: 6, codigo: "ck06", caption: "Juegos y risas", fotos: [] },
+  { id: 7, codigo: "ck07", caption: "Celebraciones", fotos: [] },
+  { id: 8, codigo: "ck08", caption: "Logros alcanzados", fotos: [] },
+  { id: 9, codigo: "ck09", caption: "Recuerdos inolvidables", fotos: [] },
+  { id: 10, codigo: "ck10", caption: "Crecimiento", fotos: [] },
+  { id: 11, codigo: "ck11", caption: "Sueños cumplidos", fotos: [] },
+  { id: 12, codigo: "ck12", caption: "Familia unida", fotos: [] },
+  { id: 13, codigo: "ck13", caption: "Orgullo y alegría", fotos: [] },
+  { id: 14, codigo: "ck14", caption: "Nuevos comienzos", fotos: [] },
+  { id: 15, codigo: "ck15", caption: "El día más importante", fotos: [] },
+  { id: 16, codigo: "ck16", caption: "Nueva etapa", fotos: [] },
+  { id: 17, codigo: "ck17", caption: "Felicidades", fotos: [] },
+  { id: 18, codigo: "ck18", caption: "Éxito total", fotos: [] },
+  { id: 19, codigo: "ck19", caption: "Gracias por todo", fotos: [] },
+  { id: 20, codigo: "ck20", caption: "Hasta siempre", fotos: [] },
+  { id: 21, codigo: "ck21", caption: "El futuro nos espera", fotos: [] }
 ];
 
 const portadas = [
@@ -39,14 +35,4 @@ const portadas = [
   "DSC_2436_copia.jpg", "DSC_2463_copia.jpg", "DSC_2495_copia.jpg",
   "DSC_2525_copia.jpg", "DSC_2553_copia.jpg", "DSC_2564_copia.jpg",
   "DSC_2578_copia.jpg", "DSC_2595_copia.jpg", "DSC_2752_copia.jpg"
-];
-
-const captions = [
-  "Primeros pasos", "Sonrisas de alegría", "Momentos especiales",
-  "Amistades forjadas", "Aprendizajes", "Juegos y risas",
-  "Celebraciones", "Logros alcanzados", "Recuerdos inolvidables",
-  "Crecimiento", "Sueños cumplidos", "Familia unida",
-  "Orgullo y alegría", "Nuevos comienzos", "El día más importante",
-  "Nueva etapa", "Felicidades", "Éxito total",
-  "Gracias por todo", "Hasta siempre", "El futuro nos espera"
 ];
